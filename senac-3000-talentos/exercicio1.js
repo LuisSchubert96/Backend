af88fdbe532a631249f6dc9c0c95d3ebe8f47528
Luís Schubert <luishvianna@outlook.com>
2024-10-24 20:37:25 -0300

    Add files via upload

--- senac-3000-talentos/exercicio1.js
+++ senac-3000-talentos/exercicio1.js
@@ -1,19 +1,29 @@
-/*
-Exercício 1) Verificação de Elegibilidade para Empréstimo
-Crie um programa que verifique se uma pessoa é elegível para um empréstimo. A pessoa deve:
-
-Ter pelo menos 21 anos de idade
-Ter um emprego estável (true ou false)
-Não ter dívidas pendentes (true ou false)
-*/
-
-let idade = 22;
-let temEmprego = true;
-let temDividasPendentes = false;
-
-// Verifique a elegibilidade para o empréstimo usando operadores lógicos
-if (temEmprego && !temDividasPendentes && idade >= 21) {
-  console.log("Elegível para empréstimo.");
-} else {
-  console.log("Não elegível para empréstimo.");
+const https = require("https")
+function buscarUsuario(){
+    const url = 'https://jsonplaceholder.typicode.com/users/1'
+    return new Promise((resolve, reject) => {
+        https
+        .get (url, (res) => {
+            let usuario = ""
+            res.on("Dados", (pedaco) => {
+                usuario += pedaco;
+              });
+      
+                res.on("end", () => {
+                try {
+                  const resultado = usuario;
+                  resolve(usuario);
+                } catch (erro) {
+                  reject("Erro ao processar os dados: " + erro);
+                }
+              });
+        })
+    })
 }
+buscarUsuario()
+  .then((usuario) => {
+    console.log("Dados do usuário:", usuario);
+  })
+  .catch((erro) => {
+    console.error("Erro ao buscar usuário:", erro);
+  });
